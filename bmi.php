1a25dc3619ec125cb0426b33bb56bebef08dc250
oishee42@gmail.com <oishee42@gmail.com>
2021-11-30 18:08:35 +0600

    bmi-calculator

---
+++ bmi.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <p>Height (in cm)</p>
+    <input type="text" id="height">
+  
+    <p>Weight (in kg)</p>
+
+    <input type="text" id="weight">
+<button onclick="document.getElementById('id01').style.display='block'" id="navlogin"></i>Login</button>
+<div id="id01" class="bmi">
+
+</div>
+</html>
